126dd3e0fd42367f2dc799e22a17cb6984d64e92
Mohammad Ghnim <mohammadghnim92@gmail.com>
2023-06-25 00:34:21 +0200

    add tags library

--- posts/models.py
+++ posts/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from taggit.managers import TaggableManager
 
 # Create your models here.
 class Author(models.Model):
     name = models.CharField(max_length=100)
     bio = models.TextField(max_length=500)
+    image = models.ImageField(upload_to='authors')
+
     def __str__(self):
         return self.name
 
@@ -12,6 +15,8 @@ class Post(models.Model):
     puplish_date = models.DateTimeField()
     content = models.TextField(max_length=15000)
     author = models.ForeignKey(Author, related_name='post_author', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='posts')
+    tags = TaggableManager()
 
     def __str__(self):
         return self.title
