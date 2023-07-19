4b2a1a6671161ca4aa374c791829cf707e5bd815
Mohammad Ghnim <mohammadghnim92@gmail.com>
2023-07-19 18:59:38 +0200

    add class based views

--- posts/views.py
+++ posts/views.py
@@ -2,37 +2,37 @@ from django.shortcuts import render, redirect
 from .models import Post
 from .forms import PostForm
 
+from django.views import generic
+
 # Create your views here.
 
-def post_list(request):
-    data = Post.objects.all()
-    return render(request, 'posts/posts.html', {'posts':data})
-
-def post_detail(request, post_id):
-    data = Post.objects.get(id=post_id)
-    return render(request, 'posts/detail.html', {'post':data})
-
-def new_post(request):
-    if request.method == 'POST':
-        form = PostForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('/blog/')
-    else:
-        form = PostForm()
-    return render(request, 'posts/new.html', {'form':form})
+class PostList(generic.ListView):      #html: post_list   context: object_list or post_list
+    model = Post
 
 
-def edit_post(request, post_id):
-    data = Post.objects.get(id=post_id)
-    if request.method == 'POST':
-        form = PostForm(request.POST, request.FILES, instance=data)
-        if form.is_valid():
-            form.save()
-            return redirect('/blog/')
-    else:
-        form = PostForm(instance=data)
-    return render(request, 'posts/edit.html', {'form':form})
+class PostDetail(generic.DetailView):      #html: post_detail   context: object or post
+    model = Post
+
+
+class PostCreate(generic.CreateView):
+    model = Post
+    fields = ['title', 'puplish_date', 'content', 'author', 'image', 'tags']
+    success_url = '/blog/'
+
+
+class PostEdit(generic.UpdateView):
+    model = Post
+    fields = ['title', 'puplish_date', 'content', 'author', 'image', 'tags']
+    success_url = '/blog/'
+    template_name = 'posts/edit.html'
+
+
+
+class PostDelete(generic.DeleteView):
+    model = Post
+    success_url = '/blog/'
+
+
 
 def delete_post(request, post_id):
     data = Post.objects.get(id=post_id)
